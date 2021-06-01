d15930982568a725bda90e9ae3197f9dc85fb0ee
Michał <michalurb8@wp.pl>
2021-06-01 09:26:24 +0200

    Reads whole product, user files now

--- readData.py
+++ readData.py
@@ -1,21 +1,17 @@
 import jsonlines
 
 
-def getData():
+def getSessionData():
     with jsonlines.open('sessions.jsonl') as reader:
         data = [[item["user_id"], item["product_id"], item["event_type"]] for item in reader]
-    # for datum in data: # print all 183 561 lines
-    #     print(datum)
     return data
 
-def getUserCount():
+def getUserIds():
     with jsonlines.open('users.jsonl') as reader:
-        data = [item for item in reader]
-        result = len(data)
-    return result
+        data = [item["user_id"] for item in reader]
+    return data
 
-def getProductCount():
+def getProductIds():
     with jsonlines.open('products.jsonl') as reader:
-        data = [item for item in reader]
-        result = len(data)
-    return result
+        data = [item["product_id"] for item in reader]
+    return data
