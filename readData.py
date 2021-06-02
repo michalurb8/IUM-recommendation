7405b2d883d9b7281b59edbd9e268430780402dd
Michał <michalurb8@wp.pl>
2021-06-02 16:54:45 +0200

    session date is now read too

--- readData.py
+++ readData.py
@@ -1,9 +1,9 @@
 import jsonlines
-
+from datetime import datetime
 
 def getSessionData():
     with jsonlines.open('sessions.jsonl') as reader:
-        data = [[item["user_id"], item["product_id"], item["event_type"]] for item in reader]
+        data = [[item["user_id"], item["product_id"], item["event_type"], _strToDate(item["timestamp"])] for item in reader]
     return data
 
 def getUserIds():
@@ -20,3 +20,9 @@ def getProductCategoryData():
     with jsonlines.open('products.jsonl') as reader:
         data = [[item["product_id"], item["category_path"]] for item in reader]
     return data
+
+
+
+def _strToDate(date: str) -> datetime:
+    result = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S')
+    return result
