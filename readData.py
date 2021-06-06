6c8a2fc94338048d035178ca30b8b863c93d8be3
Michał <michalurb8@wp.pl>
2021-06-06 18:16:17 +0200

    Add data directory for jsonl files

--- readData.py
+++ readData.py
@@ -2,22 +2,22 @@ import jsonlines
 from datetime import datetime
 
 def getSessionData():
-    with jsonlines.open('sessions.jsonl') as reader:
+    with jsonlines.open('data/sessions.jsonl') as reader:
         data = [[item["user_id"], item["product_id"], item["event_type"], _strToDate(item["timestamp"])] for item in reader]
     return data
 
 def getUserIds():
-    with jsonlines.open('users.jsonl') as reader:
+    with jsonlines.open('data/users.jsonl') as reader:
         data = [item["user_id"] for item in reader]
     return data
 
 def getProductIds():
-    with jsonlines.open('products.jsonl') as reader:
+    with jsonlines.open('data/products.jsonl') as reader:
         data = [item["product_id"] for item in reader]
     return data
 
 def getProductCategoryData():
-    with jsonlines.open('products.jsonl') as reader:
+    with jsonlines.open('data/products.jsonl') as reader:
         data = [[item["product_id"], item["category_path"]] for item in reader]
     return data
 
